543ff61ed67e2479ac96b3e8b41b54f6671e56e0
Akira Kato <a_k199@usc.edu.au>
2020-03-12 13:49:44 +1000

    week3
    week3

---
+++ js/week3.js
@@ -0,0 +1,31 @@
+//object, method
+console.log('hello');
+
+//variablre
+/*var iejjsjcf*/
+
+let number;
+number = 10;
+//const
+
+console.log(number);
+
+//primitive data types, string, number, boolean, null, underrfined
+
+//const firstName = "joe";
+//const lastName = "smith";
+//const age = 20;
+//const marriage = true;
+
+//console.log(`my name is ${firstname} ${lastName}, I'm ${age} years old.`);
+//console.log(firstName.toUpperCase());
+// firstname, lastname, age, gender, address: unit, street, sub, city, state, postcode
+
+const firstName = "Kato";
+const lastName = "Akira"
+const age = 25;
+const marrage = true;
+
+console.log(`my name is ${firstname} ${lastName}, I'm ${age} years old.`);
+console.log(firstname.toUpperCase());
+
